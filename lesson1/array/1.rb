78e9b381be1feb9bde83d4910e692db04a2be7ef
Eugene Mikhasenko <navvyua@gmail.com>
2016-10-18 17:38:23 +0300

    vars renamed

--- lesson1/array/1.rb
+++ lesson1/array/1.rb
@@ -1,11 +1,11 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-a.each { |el| puts el }
+array.each { |el| puts el }
 
-a.each { |el| puts el if el > 5 }
+array.each { |el| puts el if el > 5 }
 
-b = a.select(&:odd?)
-puts b
+odd_numbers = array.select(&:odd?)
+puts odd_numbers
 
-a << 11
-a.unshift(3)
+array << 11
+array.unshift(3)
