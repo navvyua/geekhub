3a6eb9866d2b0973b8365461168ef59ebd835710
Eugene Mikhasenko <navvyua@gmail.com>
2016-10-18 23:22:23 +0300

    Added 2 task from hash

---
+++ lesson1/hash/2.rb
@@ -0,0 +1,13 @@
+hash = {
+  'yes' => 23,
+  'b' => 'travel',
+  'yesterday' => 34,
+  :yesss => :fg,
+  try: 30,
+  key: 'some value',
+  'yesterday1' => 34,
+  'yesteryear' => 2014
+}
+
+print 'Number of keys which start with \'yes\': '
+puts hash.select { |k| k.to_s.start_with?('yes') }.count
