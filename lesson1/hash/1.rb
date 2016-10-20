660b7a91e59645b1ebba41bf1bf0b024f53eba39
Eugene Mikhasenko <navvyua@gmail.com>
2016-10-20 18:11:01 +0300

    each changed to each_with_index

--- lesson1/hash/1.rb
+++ lesson1/hash/1.rb
@@ -1,13 +1,9 @@
-def cal_fib(n)
-  n < 3 ? 1 : cal_fib(n - 1) + cal_fib(n - 2)
+def calc_fib(n)
+  n < 3 ? 1 : calc_fib(n - 1) + calc_fib(n - 2)
 end
 
 hash = {}
-i = 1
 
-(:a..:z).each do |e|
-  hash[e] = cal_fib(i)
-  i += 1
-end
+(:a..:z).each_with_index { |e, i| hash[e] = calc_fib(i + 1) }
 
 hash.each_pair { |k, v| puts "#{k}: #{v}" if v < 15 }
