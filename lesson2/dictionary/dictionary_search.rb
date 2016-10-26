9c6e18cdc9913147203627402ace7348c202de19
Eugene Mikhasenko <navvyua@gmail.com>
2016-10-26 18:06:58 +0300

    Fixed rubocop's warnings

--- lesson2/dictionary/dictionary_search.rb
+++ lesson2/dictionary/dictionary_search.rb
@@ -1,3 +1,7 @@
+# This class searching a word pairs whereby swapping
+# the last two letters of the word results in another
+# valid word.
+# Examples: [coal, cola], [minute, minuet]
 class DictionarySearch
   def initialize(file_path)
     @dictionary = {}
@@ -7,9 +11,7 @@ class DictionarySearch
       f.each_line do |line|
         line.chomp!
         next if line[-1] == line[-2] || line.size < 3
-        key = line[0..-3]
-        @dictionary[key] ||= []
-        @dictionary[key] << line[-2..-1]
+        add_word_to_dictionary(line)
       end
     end
 
@@ -19,15 +21,24 @@ class DictionarySearch
   def word_pairs
     @dictionary.each_pair do |k, v|
       v.each do |e1|
-        v.each do |e2|
-          if e1 == e2.to_s.reverse
-            @word_pairs << [k + e1, k + e2]
-            v.delete(e2)
-          end
-        end
+        v.each { |e2| compare_words(e1, e2, k, v) }
       end
     end
 
     @word_pairs
+  end
+
+  private
+
+  def add_word_to_dictionary(line)
+    key = line[0..-3]
+    @dictionary[key] ||= []
+    @dictionary[key] << line[-2..-1]
+  end
+
+  def compare_words(element1, element2, key, value)
+    return unless element1 == element2.to_s.reverse
+    @word_pairs << [key + element1, key + element2]
+    value.delete(element2)
   end
 end
--- lesson2/polynomial/polynomial.rb
+++ lesson2/polynomial/polynomial.rb
@@ -1,30 +1,61 @@
-# dasdsa
+# This class can create polynomial string from array of numbers.
+# For example: [-5, -2, 1, 0, 6]) => -5x^4-2x^3+x^2+6
 class Polynomial
   def initialize(elements = [])
-    raise ArgumentError, 'Two or more coefficients are required' if elements.size < 2
+    @elements = elements
 
-    last_zero = elements.last.zero?
-    elements.reject!(&:zero?)
-    return if elements.empty?
+    last_zero = @elements.last.zero?
+    check_number_of_coefficients
+    return if check_and_reject_zero
 
     @polynomial = []
-    elements.each_with_index do |e, i|
-      last = (i + 1) == elements.size ? true : false
+    @elements.each_with_index { |e, i| add_element(e, i) }
 
-      if e.negative?
-        @polynomial << '-'
-      else
-        @polynomial << '+' unless i.zero?
-      end
-
-      @polynomial << e.abs.to_s unless e.abs == 1 && !last
-      @polynomial << "x^#{elements.size - i}" unless last
-    end
-    @polynomial << 'x' if last_zero
-    @polynomial = @polynomial.join
+    format_polynomial(last_zero)
   end
 
   def to_s
     @polynomial ||= 0.to_s
+  end
+
+  private
+
+  def check_and_reject_zero
+    @elements.reject!(&:zero?)
+    @elements.empty?
+  end
+
+  def check_number_of_coefficients
+    return unless @elements.size < 2
+    raise ArgumentError, 'Two or more coefficients are required'
+  end
+
+  def add_sign(element, index)
+    if element.negative?
+      @polynomial << '-'
+    else
+      @polynomial << '+' unless index.zero?
+    end
+  end
+
+  def add_number(element, last)
+    @polynomial << element.abs.to_s unless element.abs == 1 && !last
+  end
+
+  def add_pow(last, index)
+    @polynomial << "x^#{@elements.size - index}" unless last
+  end
+
+  def add_element(element, index)
+    last = (index + 1) == @elements.size ? true : false
+
+    add_sign(element, index)
+    add_number(element, last)
+    add_pow(last, index)
+  end
+
+  def format_polynomial(last_zero)
+    @polynomial << 'x' if last_zero
+    @polynomial = @polynomial.join
   end
 end
