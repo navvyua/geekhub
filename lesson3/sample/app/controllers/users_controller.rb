400662508e5544b46206651d398e49630ab19099
Eugene Mikhasenko <navvyua@gmail.com>
2016-10-30 12:51:14 +0200

    Edited users controller

--- lesson3/sample/app/controllers/users_controller.rb
+++ lesson3/sample/app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -12,16 +13,35 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(email: params[:user][:email])
-    redirect_to user
+    user = User.new(user_params)
+    if user.save
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def current_user
+    @user = User.find(params[:id])
   end
 end
