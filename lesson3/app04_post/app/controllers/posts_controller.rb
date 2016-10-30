0c9ad3fa36525e4cac6387b9e5ab8cd881748520
Eugene Mikhasenko <eug.mikhasenko@gmail.com>
2016-10-31 00:22:34 +0200

    App 04 done

---
+++ lesson3/app04_post/app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :current_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to post
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
+  end
+
+  def current_post
+    @post = Post.find(params[:id])
+  end
+end
