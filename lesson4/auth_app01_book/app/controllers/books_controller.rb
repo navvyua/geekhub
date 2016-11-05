ba21df1d16a9660d529131c8bb4a0d806533e1d8
Eugene Mikhasenko <eug.mikhasenko@gmail.com>
2016-11-05 13:11:33 +0200

    App auth 01 created

---
+++ lesson4/auth_app01_book/app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  before_action :current_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+  def current_book
+    @book = Book.find(params[:id])
+  end
+end
